f5189665bc0a469a3fcbffbed791ed38e4012aa0
Ayxan23 <ayxan042223@gmail.com>
2023-04-26 03:13:24 +0400

    completed

---
+++ FrontToBack/Contexts/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using FrontToBack.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FrontToBack.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Info> Infos { get; set; }
+
+    }
+}
---
+++ FrontToBack/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using FrontToBack.Contexts;
+using FrontToBack.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontToBack.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public HomeController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        List<Info> infos = _context.Infos.ToList();
+
+        return View(infos);
+    }
+}
